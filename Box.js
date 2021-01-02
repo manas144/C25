420cc8de46ab6018eb01fa3845ec106bcc7b5a77
manas144 <74405330+manas144@users.noreply.github.com>
2021-01-02 16:58:47 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,9 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height) {
+       super(x,y,30,30);
+      this.image = loadImage ("sprites/wood1.png") 
+  }
+  display(){
+super.display();
+  }
+};
